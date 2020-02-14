0c9d498b82c3a511acfe15526005f73432db4b59
sunmu <musun@faceunity.com>
2020-02-14 16:31:50 +0800

    update v6.6.1

--- FULiveDemo/Controller/FUBeautyViewController.m
+++ FULiveDemo/Controller/FUBeautyViewController.m
@@ -29,8 +29,6 @@
 @property (weak) IBOutlet NSTextField *mFilterTef;
 /* 滤镜程度滑动 */
 @property (weak) IBOutlet NSSlider *mFilterSlider;
-/* 风格集合控制器 */
-@property (weak) IBOutlet NSCollectionView *mMakeupCollectionView;
 /* 风格程度滑动 */
 @property (weak) IBOutlet NSSlider *mMakeupSlider;
 /* 风格程度输入 */
@@ -52,9 +50,6 @@
     _mFilterCollectionView.delegate = self;
     _mFilterCollectionView.dataSource = self;
     
-    _mMakeupCollectionView.dataSource = self;
-    _mMakeupCollectionView.delegate = self;
-    
     //初始状态
     NSUInteger indexes[2] = {0,0};
     NSIndexPath *indexPath = [[NSIndexPath alloc] initWithIndexes:indexes length:sizeof(indexes)/sizeof(NSUInteger)];
@@ -62,8 +57,6 @@
   //  [self.mStyleCollectionView selectItemsAtIndexPaths:[NSSet setWithObject:indexPath] scrollPosition:NSCollectionViewScrollPositionNone];
     _mFilterSlider.intValue = [FUAppDataCenter shareManager].filterModelArray[indexPath.item].value * 100;
     _mFilterTef.stringValue = [NSString stringWithFormat:@"%d",_mFilterSlider.intValue];
-    _mMakeupSlider.intValue = [FUAppDataCenter shareManager].makeupModelArray[indexPath.item].value * 100;
-    _mMakeupSlider.stringValue = [NSString stringWithFormat:@"%d",_mMakeupSlider.intValue];
     
 }
 
@@ -83,8 +76,6 @@
     [self.mFilterCollectionView setWantsLayer:YES];
     self.mFilterCollectionView.layer.backgroundColor = FUConstManager.colorForBackground_card.CGColor;
     
-    [self.mMakeupCollectionView setWantsLayer:YES];
-    self.mMakeupCollectionView.layer.backgroundColor = FUConstManager.colorForBackground_card.CGColor;
         
     [self.mItemBarView setWantsLayer:YES];
     self.mItemBarView.layer.backgroundColor = FUConstManager.colorForBackground_page.CGColor;
@@ -189,21 +180,6 @@
         [FUAppDataCenter shareManager].filterModelArray[_selectFilterIndex].value = sender.floatValue/100;
         //调整sdk参数
         [[FUManager shareManager] changeParamsStr:[FUAppDataCenter shareManager].filterModelArray[_selectFilterIndex].valueSdkStr index:0 value:@([FUAppDataCenter shareManager].filterModelArray[_selectFilterIndex].value)];
-        
-    }else if (sender == _mMakeupSlider){
-        [_mMakeupTxf setStringValue:[NSString stringWithFormat:@"%d",sender.intValue]];
-        FUMakeupModle *model = [FUAppDataCenter shareManager].makeupModelArray[_selectStyleIndex];
-        
-        model.value = sender.floatValue/100;
-        model.selectedFilterLevel = sender.floatValue/100;
-        //调整sdk参数
-        for (int i = 0; i < model.makeups.count; i ++) {
-            if (!model.makeups[i].namaValueStr || [model.makeups[i].namaValueStr isEqualToString:@""]) {
-                return;
-            }
-            [[FUManager shareManager] setMakeupItemIntensity:model.value * model.makeups[i].value param:model.makeups[i].namaValueStr];
-        }
-        [[FUManager shareManager] changeParamsStr:@"filter_level" index:0 value:@(model.selectedFilterLevel)];
     }
 }
 
@@ -215,21 +191,6 @@
             [FUAppDataCenter shareManager].filterModelArray[_selectFilterIndex].value = sender.floatValue/100;
             //调整sdk参数
             [[FUManager shareManager] changeParamsStr:[FUAppDataCenter shareManager].filterModelArray[_selectFilterIndex].valueSdkStr index:0 value:@([FUAppDataCenter shareManager].filterModelArray[_selectFilterIndex].value)];
-        }else if (sender == _mMakeupTxf){
-            _mMakeupSlider.intValue = sender.intValue;
-            FUMakeupModle *model = [FUAppDataCenter shareManager].makeupModelArray[_selectStyleIndex];
-            
-            model.value = sender.floatValue/100;
-            model.selectedFilterLevel = sender.floatValue/100;
-            //调整sdk参数
-            for (int i = 0; i < model.makeups.count; i ++) {
-                if (!model.makeups[i].namaValueStr || [model.makeups[i].namaValueStr isEqualToString:@""]) {
-                    return;
-                }
-                [[FUManager shareManager] setMakeupItemIntensity:model.value * model.makeups[i].value param:model.makeups[i].namaValueStr];
-            }
-            [[FUManager shareManager] changeParamsStr:@"filter_level" index:0 value:@(model.selectedFilterLevel)];
-            
         }
     }else{
         if (sender == _mFilterTef) {
@@ -256,21 +217,13 @@
 
 - (NSInteger)collectionView:(NSCollectionView *)collectionView
      numberOfItemsInSection:(NSInteger)section {
-    if (collectionView == _mMakeupCollectionView) {
-        return [FUAppDataCenter shareManager].makeupModelArray.count;
-    }else{
-         return [FUAppDataCenter shareManager].filterModelArray.count;
-    }
+        return [FUAppDataCenter shareManager].filterModelArray.count;
 }
 
 - (NSCollectionViewItem *)collectionView:(NSCollectionView *)collectionView itemForRepresentedObjectAtIndexPath:(NSIndexPath *)indexPath {
     
     FUFilterViewItem *item = [collectionView makeItemWithIdentifier:@"FUFilterViewItem" forIndexPath:indexPath];
-    if (collectionView == _mMakeupCollectionView) {
-        item.makeupModel = [FUAppDataCenter shareManager].makeupModelArray[indexPath.item];
-    }else{
-        item.model = [FUAppDataCenter shareManager].filterModelArray[indexPath.item];
-    }
+    item.model = [FUAppDataCenter shareManager].filterModelArray[indexPath.item];
     
     return item;
 }
@@ -289,48 +242,6 @@
 
 - (void)collectionView:(NSCollectionView *)collectionView didSelectItemsAtIndexPaths:(NSSet<NSIndexPath *> *)indexPaths{
      NSIndexPath *indexPath = indexPaths.anyObject;
-    if (collectionView == _mMakeupCollectionView) {
-        [_mFilterCollectionView deselectAll:nil];
-        [_mFilterCollectionView reloadData];
-        _selectStyleIndex = indexPath.item;
-        
-        FUMakeupModle *modle = [FUAppDataCenter shareManager].makeupModelArray[indexPath.item];
-        _mMakeupSlider.intValue = modle.value * 100;
-         [_mMakeupTxf setStringValue:[NSString stringWithFormat:@"%d",_mMakeupSlider.intValue]];
-        
-        //原图不显示滑条
-        if (indexPath.item == 0) {
-            _mMakeupSlider.hidden = YES;
-            _mMakeupTxf.hidden = YES;
-        }else{
-            _mMakeupSlider.hidden = NO;
-            _mMakeupTxf.hidden = NO;
-            
-            [[FUManager shareManager] changeParamsStr:@"filter_name" index:0 value:modle.selectedFilter];
-            [[FUManager shareManager] changeParamsStr:@"filter_level" index:0 value:@(modle.selectedFilterLevel)];
-        }
-        
-        //调整sdk参数
-        for (int i = 0; i < modle.makeups.count; i ++) {
-            FUSingleMakeupModel *sModel = modle.makeups[i];
-            
-            if (i == 0) {
-                NSArray *rgba = [self jsonToLipRgbaArrayResName:sModel.namaImgStr];
-                double lip[4] = {[rgba[0] doubleValue],[rgba[1] doubleValue],[rgba[2] doubleValue],[rgba[3] doubleValue]};
-                [[FUManager shareManager] setMakeupItemLipstick:lip];
-            }else{
-                [[FUManager shareManager] setMakeupItemIntensity:sModel.value * modle.value param:sModel.namaValueStr];
-                NSImage *namaImage = [NSImage imageNamed:sModel.namaImgStr];
-                if (!namaImage) {
-                    continue;
-                }
-                [[FUManager shareManager] setMakeupItemParamImage:namaImage param:sModel.namaTypeStr];
-            }
-        }
-        
-    }else{
-        [_mMakeupCollectionView deselectAll:nil];
-        [_mMakeupCollectionView reloadData];
         //原图不显示滑条
         if (indexPath.item == 0) {
             _mFilterSlider.hidden = YES;
@@ -346,7 +257,6 @@
         //调整sdk参数
        [[FUManager shareManager] changeParamsStr:[FUAppDataCenter shareManager].filterModelArray[indexPath.item].sdkStr index:0 value:[[FUAppDataCenter shareManager].filterModelArray[indexPath.item].styleStr lowercaseString]];
        [[FUManager shareManager] changeParamsStr:[FUAppDataCenter shareManager].filterModelArray[indexPath.item].valueSdkStr index:0 value:@([FUAppDataCenter shareManager].filterModelArray[indexPath.item].value)];
-    }
 
 }
 
